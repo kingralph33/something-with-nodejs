5c5276f11e23eb7dc290526e7b7b06ca03be1096
Ralph King Jr <15459559+kingralph33@users.noreply.github.com>
2023-03-30 15:28:04 -0400

    update server text

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ const app: Express = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World! My name is Ralph and I am married and have a daughter and I am married and have a daughte. I love them')
+  res.send('Hello World! My name is Ralph') 
 })
 
 app.listen(port, () => {
